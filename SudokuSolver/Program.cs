c5205874475968033bf943e70aa4bb305c5c2d7c
Adam Bloom <admanb@gmail.com>
2019-12-30 14:36:13 -0800

    initial solver

---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(ConvertToSquare(0, 0));
+            Console.WriteLine(ConvertToSquare(7, 7));
+            Console.WriteLine(ConvertToSquare(5, 5));
+            Console.WriteLine(ConvertToSquare(7, 0));
+
+            List<List<int>> puzzle = new List<List<int>>
+            {
+                new List<int> { 0, 3, 0, 0, 0, 0, 9, 0, 6 },
+                new List<int> { 6, 0, 2, 9, 4, 3, 8, 5, 1 },
+                new List<int> { 0, 0, 0, 0, 0, 0, 0, 7, 3 },
+                new List<int> { 3, 9, 1, 7, 0, 0, 0, 6, 8 },
+                new List<int> { 0, 0, 0, 0, 1, 0, 0, 4, 2 },
+                new List<int> { 4, 0, 0, 0, 8, 6, 0, 0, 0 },
+                new List<int> { 9, 4, 7, 0, 3, 0, 0, 0, 0 },
+                new List<int> { 0, 1, 6, 0, 9, 5, 0, 3, 0 },
+                new List<int> { 8, 0, 0, 0, 6, 7, 0, 0, 9 },
+            };
+
+            var solver = new Solver(puzzle);
+            solver.Print();
+        }
+
+        public static int ConvertToSquare(int col, int row)
+        {
+            return col / 3 + 3 * (row / 3);
+        }
+    }
+}
---
+++ SudokuSolver/Solver.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver
+{
+    class Solver
+    {
+        public List<List<int>> Puzzle { get; set; }
+        public List<List<bool>> RowValues { get; set; } = new List<List<bool>>();
+        public List<List<bool>> ColumnValues { get; set; } = new List<List<bool>>();
+        public List<List<bool>> SquareValues { get; set; } = new List<List<bool>>();
+
+        public Solver(List<List<int>> puzzle)
+        {
+            for(var i = 0; i < 9; i++)
+            {
+                RowValues.Add(new List<bool> { false, false, false, false, false, false, false, false, false, false });
+                ColumnValues.Add(new List<bool> { false, false, false, false, false, false, false, false, false, false });
+                SquareValues.Add(new List<bool> { false, false, false, false, false, false, false, false, false, false });
+            }
+
+            this.Puzzle = puzzle;
+            this.ConfigureInitialPossibilities();
+            this.Solve();
+        }
+
+        public void Solve()
+        {
+            var solved = false;
+            while (!solved)
+            {
+                solved = true;
+                for (var row = 0; row < 9; row++)
+                {
+                    for (var col = 0; col < 9; col++)
+                    {
+                        var val = Puzzle[row][col];
+                        if (val == 0)
+                        {
+                            solved = false;
+                            List<int> possibilites = CalculatePossibilities(col, row);
+                            if (possibilites.Count == 1)
+                            {
+                                Puzzle[row][col] = possibilites[0];
+                                RowValues[row][possibilites[0]] = true;
+                                ColumnValues[col][possibilites[0]] = true;
+                                SquareValues[ConvertToSquare(col, row)][possibilites[0]] = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ConfigureInitialPossibilities()
+        {
+            for (var row = 0; row < 9; row++)
+            {
+                for (var col = 0; col < 9; col++)
+                {
+                    var val = Puzzle[row][col];
+                    if (val != 0)
+                    {
+                        RowValues[row][val] = true;
+                        ColumnValues[col][val] = true;
+                        SquareValues[ConvertToSquare(col, row)][val] = true;
+                    }
+                }
+            }
+        }
+
+        public List<int> CalculatePossibilities(int col, int row)
+        {
+            var result = new List<int>();
+            var square = ConvertToSquare(col, row);
+
+            for (var i = 1; i <= 9; i++)
+            {
+                if (!RowValues[row][i] && !ColumnValues[col][i] && !SquareValues[square][i])
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+
+        public int ConvertToSquare(int col, int row)
+        {
+            return col / 3 + 3 * (row / 3);
+        }
+
+        public void Print()
+        {
+            for (var row = 0; row < 9; row++)
+            {
+                for (var col = 0; col < 9; col++)
+                {
+                    Console.Write(Puzzle[row][col] + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+    }
+}
